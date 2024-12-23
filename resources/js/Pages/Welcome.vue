5f90baf86b42d24fe789ff9d74027683698b4f6c
c0dy49lrra <cluerra@gmail.com>
2024-12-23 10:43:18 -0800

    paige update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -8,6 +8,25 @@ import { Inertia } from '@inertiajs/inertia';
 
 const password = ref(''); // Bind the password input
 
+const showSection1 = ref(true);
+const showSection2 = ref(false);
+const showSection3 = ref(false);
+const showSection4 = ref(false);
+const showSection5 = ref(false);
+
+const nextSection = (currentSection) => {
+    if (currentSection === 1) {
+        showSection2.value = true;
+    } else if (currentSection === 2) {
+        showSection3.value = true;
+    } else if (currentSection === 3) {
+        showSection4.value = true;
+    }
+    else if (currentSection === 4) {
+        showSection5.value = true;
+    }
+};
+
 const enter = async () => {
     if (!password.value) {
         alert('Please enter a password.');
@@ -39,10 +58,38 @@ const enter = async () => {
         >
             <div class="relative w-full max-w-2xl px-6 ">
                 <main class="mt-6">
+
+                    <p class="bg-green-700 p-3 rounded-xl text-white my-2">To use your gift you must solve the trivia below</p>
+
                     <div class="grid gap-6 lg:grid-cols-1 lg:gap-8">
                         <TextInput v-model="password" type="password" placeholder="Enter Password"></TextInput>
                         <PrimaryButton @click="enter();">Enter</PrimaryButton>
                     </div>
+
+
+                    <!-- Trivia Sections -->
+                <div class="mt-4" v-if="showSection1">
+                    <p class="bg-green-700 p-3 rounded-xl text-white my-2">"Taylor Swift has released numerous albums. How many of those albums were released in the year 2008?"</p>
+                    <PrimaryButton @click="nextSection(1)">Next</PrimaryButton>
+                </div>
+                <div v-if="showSection2">
+                    <p class="bg-green-700 p-3 rounded-xl text-white my-2">(2 × 3) - 4 + (5 / 5)</p>
+                    <PrimaryButton @click="nextSection(2)">Next</PrimaryButton>
+                </div>
+
+                <div v-if="showSection3">
+                    <p class="bg-green-700 p-3 rounded-xl text-white my-2">I am what divides and unites, Two parts together in one sight. I’m the pair in a duo’s play, The number of feet to walk a way. What am I?</p>
+                    <PrimaryButton @click="nextSection(3)">Next</PrimaryButton>
+
+                </div>
+
+                <div v-if="showSection4">
+                    <p class="bg-green-700 p-3 rounded-xl text-white my-2">We started with none, but now we can have no more. You didn’t want one, but now you want more. How many do we have?</p>
+                    <PrimaryButton @click="nextSection(4)">Next</PrimaryButton>
+                </div>
+                <div v-if="showSection5">
+                    <p class="bg-green-700 p-3 rounded-xl text-white my-2">Hint: Change the order to make sense </p>
+                </div>
                 </main>
                  <header
                     class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3"
